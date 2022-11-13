0f30960323be1073d19314037d2747c6cc161d30
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2022-11-13 18:02:40 -0400

    Added isPalindrome and genPermutations

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -50,20 +51,116 @@ long recFibo2(unsigned int n) {
     return recFibo2Helper(n, 0, 1);
 }
 
+
+bool iterIsPalindrome(string w){
+
+    for (int i=0; i<w.size()/2; i++) {
+        if (w[i] != w[w.size() - i - 1]) return false;
+    }
+    return true;
+}
+
+
+bool recIsPalindrome(string w) {
+
+    if (w.size() <= 1) return true;
+    return ((w[0] == w[w.size() - 1]) && recIsPalindrome(w.substr(1,w.size()-2)));
+
+}
+
+vector<string> genPermutations(string w) {
+
+    vector<string> result;
+    if (w.size() <=1) {
+        result.push_back(w);
+        return result;
+    }
+    for (int i=0; i<w.size(); i++) {
+        string letterRemoved = w.substr(i,1);
+        string shorterWord = w.substr(0,i) + w.substr(i+1,w.size()-i-1);
+        vector<string> shorterPerms = genPermutations(shorterWord);
+        for (int j=0; j<shorterPerms.size(); j++) {
+            result.push_back(letterRemoved + shorterPerms[j]);
+        }
+
+    }
+    return result;
+
+}
+
+
+
+int maze[6][6] =
+    {
+        {2, 2, 2, 0, 0, 0},
+        {0, 0, 2, 0, 0, 0},
+        {0, 0, 3, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0}};
+
+bool findCheese(int y, int x)
+{
+    if ((x < 0) || (x > 5)) {
+        // row outside maze
+        return false;
+    }
+    if ((y < 0) || (y > 5)) {
+        // colummn outside maze
+        return false;
+    }
+    if (maze[y][x] == 3) {
+        // Cheese found
+        return true;
+    }
+    if (maze[y][x] == 0) {
+        // Wall found
+        return false;
+    }
+    if (maze[y][x] == 1) {
+        // Space previously visited
+        return false;
+    }
+    maze[y][x] = 1; // Mark space visited. Prevents starvation
+    if (findCheese(y-1,x)) {
+        return true;
+    }
+    if (findCheese(y,x+1)) {
+        return true;
+    }
+    if (findCheese(y+1,x)) {
+        return true;
+    }
+    if (findCheese(y,x-1)) {
+        return true;
+    }
+    return false;
+}
+
 int main() {
 
-    for (int i = 0; i < 10; i++)
-        cout << "iterFactorial(" << i << ") = " << iterFactorial(i) << endl;
+    // for (int i = 0; i < 10; i++)
+    //     cout << "iterFactorial(" << i << ") = " << iterFactorial(i) << endl;
 
-    for (int i = 0; i < 10; i++)
-        cout << "recFactorial(" << i << ") = " << recFactorial(i) << endl;
+    // for (int i = 0; i < 10; i++)
+    //     cout << "recFactorial(" << i << ") = " << recFactorial(i) << endl;
 
-    for (int i = 0; i <= 50; i++)
-        cout << "iterFibo(" << i << ") = " << iterFibo(i) << endl;
+    // for (int i = 0; i <= 50; i++)
+    //     cout << "iterFibo(" << i << ") = " << iterFibo(i) << endl;
 
-    for (int i = 0; i <= 50; i++)
-        cout << "recFibo2(" << i << ") = " << recFibo2(i) << endl;
+    // for (int i = 0; i <= 50; i++)
+    //     cout << "recFibo2(" << i << ") = " << recFibo2(i) << endl;
 
-    for (int i = 0; i <= 50; i++)
-        cout << "recFibo(" << i << ") = " << recFibo(i) << endl;
+    // for (int i = 0; i <= 50; i++)
+    //     cout << "recFibo(" << i << ") = " << recFibo(i) << endl;
+
+    cout << iterIsPalindrome("radar") << endl;
+
+    vector<string> eatPerms = genPermutations("Bienvenido");
+
+    for (int i=0; i<eatPerms.size(); i++) {
+        cout << "Next word is: " << eatPerms[i] << endl;
+    }
+
+    exit(0);
 }
